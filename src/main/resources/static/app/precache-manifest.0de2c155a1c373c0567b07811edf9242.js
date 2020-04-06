97e4c4c62808b4d544cf6950ad86559eef44bc89
Yi Wei <pangyiwei@gmail.com>
2020-04-06 15:09:01 +0800

    Add Frontend built html

---
+++ src/main/resources/static/app/precache-manifest.0de2c155a1c373c0567b07811edf9242.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f090c5bec3fed8400b59392af298baff",
+    "url": "/app/index.html"
+  },
+  {
+    "revision": "12a77d3b8e220ec36766",
+    "url": "/app/static/css/main.816dea7a.chunk.css"
+  },
+  {
+    "revision": "ab4f6f13c9c8ab776270",
+    "url": "/app/static/js/2.9d7021ab.chunk.js"
+  },
+  {
+    "revision": "d466ab9648f013dea081956fc7c904f3",
+    "url": "/app/static/js/2.9d7021ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12a77d3b8e220ec36766",
+    "url": "/app/static/js/main.7adb0716.chunk.js"
+  },
+  {
+    "revision": "ddaee2bf92d4e82766d5",
+    "url": "/app/static/js/runtime-main.37261044.js"
+  },
+  {
+    "revision": "fb52abb7baff08587594ba07d135bc2c",
+    "url": "/app/static/media/bus.fb52abb7.png"
+  },
+  {
+    "revision": "a564af4e35847805b9ff1c5010cb88ce",
+    "url": "/app/static/media/bus2.a564af4e.jpg"
+  },
+  {
+    "revision": "d586706c27e37b866653635ab8c968fb",
+    "url": "/app/static/media/signin-image.d586706c.jpg"
+  },
+  {
+    "revision": "75eb7c4e5d58a0a3f0d387b9bc44e080",
+    "url": "/app/static/media/signup-image.75eb7c4e.jpg"
+  }
+]);
